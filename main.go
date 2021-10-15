2a26592d066b3dab0aa1d4884addd4a618ea2747
yoonhero <yoonhero06@naver.com>
2021-10-15 23:19:26 +0900

    deploy: Heroku

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/yoonhero/baljaguk_server/db"
 	"github.com/yoonhero/baljaguk_server/rest"
+	"github.com/yoonhero/baljaguk_server/utils"
 )
 
 func main() {
 	// os.Setenv("PORT", "4000")
 
-	// port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	// close db to protect db file data
 	defer db.CloseSqlDB()
 	db.InitPostgresDB()
 
-	// sv, err := strconv.Atoi(port)
-	// utils.HandleErr(err)
-	rest.Start(4000)
+	sv, err := strconv.Atoi(port)
+	utils.HandleErr(err)
+	rest.Start(sv)
 }
